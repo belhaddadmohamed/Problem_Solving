56413ae169f5609db6f2d767af597d8f06ec31c2
belhaddadMohamed <belhaddad.mohamed7@gmail.com>
2024-02-09 15:02:03 +0100

    Update fibo.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,43 +1,51 @@
-# 1st method : Recursion
-def Fibonacci_unMemoized(n):
+# 1st Method : unMemoized
+def Fibo_unMemo(n):
 	if n < 0:
-		print("Incorrect input")
+		print("N should be positive")
 	elif n == 0:
 		return 0
 	elif n == 1 or n == 2:
 		return 1
 	else:
-		return Fibonacci_unMemoized(n-1) + Fibonacci_unMemoized(n-2)
+		return Fibo_unMemo(n-1) + Fibo_unMemo(n-2)
 
 
 
-# 2nd method : Dynamic programming
+# 2nd Method : Memoized
 cache = {0:0, 1:1}
 def Fibo_Memo(n):
-	if n in cache:
+	if n < 0:
+		print("N should be positive")
+	elif n in cache:
 		return cache[n]
-		
-	cache[n] = Fibo_Memo[n-1] + Fibo_Memo[n-2]
-	return cache[n]
+	else:
+		cache[n] = Fibo_Memo(n-1) + Fibo_Memo(n-2)
+
+		return cache[n]
 	
 
 
-# Execution
+
+# Execution ===============================================================
 from time import time
-print("Fn \t Valeur \t Temps_sans_mémoisation \t Temps_avec_mémoisation")
+
+print("Fibo | Value | unMemo_Time | Memo_Time")
 print("__________________________________")
-for a in range(30,50):
+
+for a in range(30, 40):
+	# unMemo_Time
 	t0 = time()
-	s = Fibonacci_unMemoized(a)
+	s = Fibo_unMemo(a)
 	t1 = time()
 	temp1 = t1 - t0
 
+	# Memo_Time
 	t2 = time()
-	s = Fibonacci_Memoized(a)
+	s = Fibo_Memo(a)
 	t3 = time()
 	temp2 = t2 - t3
 
-	print("%d \t %d \t %0.5f \t %0.5f" % (s, a ,  t1 - t0, t2 - t3))
+	print("%d | %d | %0.5f | %0.5f" % (s, a ,  t1 - t0, t2 - t3))
 
 
 
